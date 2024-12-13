cb0e54d663bb894c5671ebab4be8f346389f89ad
Tiril Marie Pedersen Løken <94623622+Tavyah@users.noreply.github.com>
2024-12-13 16:21:38 +0100

    part 2  day 3 almost complete

--- 2024/Day 3/main.py
+++ 2024/Day 3/main.py
@@ -14,15 +14,43 @@ INPUT_FILENAME = 'input_data.txt'
 #make_txt_file(website_content, INPUT_FILENAME, fp.get_current_filepath())
 
 text = reading_txt_data(INPUT_FILENAME, fp.get_current_filepath())
-regex_string = 'mul\(\d+,\d+\)'
+
+regex_string = "do\(\).*?don't\(\)"
 matches = re.findall(regex_string, text)
+#print(matches)
+
+do_matches = []
+
+# TODO: FIKSE sånn at den får riktig i lista, surrer fælt nu
+
+regex_do_string = "do\(\).*?do\(\)"
+for i in range(0, len(matches)):
+    clean_matches = re.findall(regex_do_string, matches[i])
+    print(clean_matches)
+    if not clean_matches:
+        do_matches.append(matches[i])
+    else:
+        for j in clean_matches:
+            do_matches.append(j)
+
+print(do_matches)
 
 total = 0
-for i in matches:
-    i = i.strip('mul(')
-    i = i.strip(')')
-    number_1, number_2 = i.split(',')
-    total += int(number_1) * int(number_2)
+
+regex_strip = 'mul\(\d+,\d+\)'
+first_mul = re.search(regex_strip, text)
+do_matches.append(first_mul.group())
+
+for i in range(0, len(do_matches)):
+    match = re.findall(regex_strip, do_matches[i])
+
+    for each_match in match:
+        each_match = each_match.strip('mul(')
+        each_match = each_match.strip(')')
+        number_1, number_2 = each_match.split(',')
+
+        total += int(number_1) * int(number_2)
+
 
 print(total)
-make_txt_file(str(total), 'answer_part_1.txt', fp.get_current_filepath())
+make_txt_file(str(total), 'answer_part_2.txt', fp.get_current_filepath())
