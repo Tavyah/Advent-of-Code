4c3361e8eaae7265f115780676d9a760e6d66163
Tavyah <94623622+Tavyah@users.noreply.github.com>
2024-12-16 08:37:39 +0100

    ongoing part 1 day 4

---
+++ 2024/Day 4/main.py
@@ -0,0 +1,20 @@
+import os, sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'Modules')))
+
+from web_browser_helper import *
+from filehandler_helper import *
+import filepath as fp
+
+URL = 'https://adventofcode.com/2024/day/4/input'
+INPUT_FILE = 'input_data.txt'
+#content = scrape_input_site(URL)
+#make_txt_file(content, INPUT_FILE, fp.get_current_filepath())
+
+text = reading_txt_data('sample_data.txt', fp.get_current_filepath())
+print(text)
+
+
+cleaned_text = [line.replace('\\n', '') for line in text]
+
+print(cleaned_text)
